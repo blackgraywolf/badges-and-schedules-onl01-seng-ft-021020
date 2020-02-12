f3482dbebd1171f8f254273f61cb26f3da944213
industrious-generator-6081 <graywolf.niehaus@live.com>
2020-02-12 23:57:15 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,27 @@
-# Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  array.map do |name| "Hello, my name is #{name}."
+  end
+end
+
+#assign_rooms
+def assign_rooms(array)
+	array.map do |name, index|"Hello #{name}! You'll be assigned to room #{index + 1}"
+	end
+end
+
+
+
+def assign_rooms(array)
+  nuarray = []
+  counter = 1
+  array.each do |name|
+    nuarray.push("Hello, #{name}! You'll be assigned to room #{counter}!")
+    counter += 1
+  end
+  return nuarray
+end
+
