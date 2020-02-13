b4e9162ba3d294c74b3666abdd9cb5201c8946a2
industrious-generator-6081 <graywolf.niehaus@live.com>
2020-02-13 00:06:18 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,32 +1,20 @@
-#badge_maker
 def badge_maker(name)
   return "Hello, my name is #{name}."
 end
 
-#batch_badge_creator
-guests = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 def batch_badge_creator(array)
-  array.map do |name|
-    "Hello, my name is #{name}."
+  array.map do |name| "Hello, my name is #{name}."
   end
 end
 
 #assign_rooms
-def assign_rooms(array1)
-	array1.each_with_index.map do |name, index| 
-		"Hello #{name}! You'll be assigned to room #{index + 1}"
+def assign_rooms(name)
+  index = 1
+	array.map do |name, index|"Hello #{name}! You'll be assigned to room #{index + 1}"
 	end
 end
 
-def printer(guests)
-  
-  batch_badge_creator(guests).each do |badge|
-    puts badge
-  end
-
-  assign_rooms(guests).each do |room|
-    puts room
-    end
-end
-
-printer(guests)
+def printer(array)
+  batch_badge_creator(array).each do |id|
+    puts id
+  end 
